72c45e8e01771e9950f39cd6b7f12f47463ff2a1
Artyom <buraev22@yandex.ru>
2020-10-31 14:17:10 +0400

    Не хватило времени выполнить 7 задание без звездочки

---
+++ Homework2.1/Homework2.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Написать метод подсчета количества цифр числа.
+            // Бураев
+            int a, b, c;
+            Console.Write("Введите первое  число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе  число: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите третье  число: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if(a < b && a < c)
+            {
+                Console.Write($"Минимальное число: {a}");
+            }
+            else
+            {
+                if(c < b)
+                {
+                    Console.Write($"Минимальное число: {c}");
+                }
+                else
+                {
+                    Console.Write($"Минимальное число: {b}");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Homework2.1/Homework2.4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2._4
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            //4. Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль.
+            //На выходе истина, если прошел авторизацию, и ложь, если не прошел(Логин: root, Password: GeekBrains). 
+            //Используя метод проверки логина и пароля, написать программу:
+            //пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает.
+            //С помощью цикла do while ограничить ввод пароля тремя попытками.
+            //Бураев
+
+
+            int count = 0;
+            do
+            {
+                Console.Write("Введите логин: ");
+                string login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                string password = Console.ReadLine();
+                if (login == "root" && password == "GeekBrains")
+                {
+                    Console.WriteLine("Вы вошли в систему");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неверные данные");
+                    count++;
+                }
+            } while (count < 3);
+            Console.ReadLine();
+        }
+    }
+}
