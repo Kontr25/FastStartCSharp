ec663a7c094d75aa95848e2f6b9d32ae1ce9f3f7
Artyom <buraev22@yandex.ru>
2020-11-23 22:18:05 +0400

    дз8

---
+++ Homework8.1/йцу/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace йцу
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type t = typeof(DateTime);
+            foreach (var sv in t.GetProperties())
+                Console.WriteLine(sv.Name);
+            Console.ReadLine();
+        }
+    }
+}
